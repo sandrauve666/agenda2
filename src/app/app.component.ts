c2f52f6418f3e5504f309ebcb864a40c20244138
sandrauve <sandrauve666@gmail.com>
2024-09-09 20:20:58 +0200

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from "./components/cabecera/cabecera.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceraComponent, FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
